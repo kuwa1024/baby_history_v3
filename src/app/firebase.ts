9255ecd780dfb6298c4fa0c791ab49979fdfe97a
kuwa <t.kuwazaki1024@gmail.com>
2024-10-18 01:11:02 +0900

    デプロイ機能実装

---
+++ src/app/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDGRouwpP21AJdfdg9KhJzIlJFciKpPgVU",
+  authDomain: "baby-history2.firebaseapp.com",
+  projectId: "baby-history2",
+  storageBucket: "baby-history2.appspot.com",
+  messagingSenderId: "633669089889",
+  appId: "1:633669089889:web:a9a171dbc51ded1c190820",
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
