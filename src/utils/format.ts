023c266560e7efe95a8fa3e92cad69a9f3cdbc54
kuwa <t.kuwazaki1024@gmail.com>
2025-02-16 00:12:28 +0900

    日付フォーマット調整

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,5 +1,5 @@
+import dayjs from 'dayjs';
+
 export const formatDate = (date: string) => {
-  const parsedDate: Date = new Date(date);
-  const padZero = (num: number) => (num < 10 ? `0${num}` : num);
-  return `${padZero(parsedDate.getMonth() + 1)}/${padZero(parsedDate.getDate())} ${padZero(parsedDate.getHours())}:${padZero(parsedDate.getMinutes())}`;
+  return dayjs(date).format('MM/DD HH:mm');
 };
